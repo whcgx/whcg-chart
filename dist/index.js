83f1c286c78124f2e44c0361b9df197ead0037cd
Andersh75 <andersh75@gmail.com>
2018-09-12 21:18:38 +0200

    build

--- dist/index.js
+++ dist/index.js
@@ -13,16 +13,24 @@ var polymerElement_js = require('@polymer/polymer/polymer-element.js');
 
 class WhcgChart extends polymerElement_js.PolymerElement {
     static get template() {
-        return polymerElement_js.html `
+        return polymerElement_js.html`
         <style>
-        :host {
-            display: inline-block;
+        .thediv {
+            --_lumo-grid-border-color: var(--whcg-shade-20pct);
+            --_lumo-grid-secondary-border-color: var(--whcg-shade-10pct);
+            --_lumo-grid-border-width: 1px;
+            --_lumo-grid-selected-row-color: var(--parmaco-primary-color-10pct);
+            font-size: var(--parmaco-font-size-m);
+            border: var(--_lumo-grid-border-width) solid var(--_lumo-grid-border-color);
+            border-radius: 5px 5px 4px 4px;
+            background-color: var(--whcg-shade-10pct);
         }
+
         </style>
-    <div>
+    <div class="thediv">
         <canvas id="myChart" width$="{{width}}" height$="{{height}}"></canvas>
     </div>
-        `
+        `;
     }
     static get properties() {
         return {
@@ -81,23 +89,48 @@ class WhcgChart extends polymerElement_js.PolymerElement {
     }
 
     _chartjsonChanged() {
-        console.log('chartJsonChanged');
+        console.log("chartJsonChanged");
+        
         this._chartJs(JSON.parse(this.chartjson));
+        // setInterval(function() {
+        //     console.log(this.thechart);
+        // }, 5000);
     }
 
     _chartJs(data) {
         console.log('data');
         console.log(data);
         var ctx = this.$.myChart;
-        var myChart = new Chart(ctx, {
+        console.log("ctx");
+        console.log(ctx);
+        
+        //Chart.defaults.global.defaultFontColor = 'white';
+        //Chart.defaults.global.elements.rectangle.backgroundColor = 'hsla(24, 70%, 50%, 1)';
+        //Chart.defaults.global.elements.rectangle.borderColor = '#FFFFFF';
+        //Chart.defaults.global.elements.rectangle.borderWidth = 1;
+
+
+        //ctx.style.backgroundColor = "hsla(214, 50%, 22%, 0.1)";
+
+        console.log('this');
+        console.log(this);
+
+        if (this.thechart != null) {
+            this.thechart.destroy();
+        }
+
+        this.thechart = new Chart(ctx, {
             type: this.type,
             data: data,
+
             options: {
                 legend: {
                     position: this.legendposition,
                     labels: {
-                        fontFamily: this.legendfontfamily,
-                        fontSize: this.legendfontsize
+                        fontFamily: "'Exo 2', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+                        fontColor: '#FFFFFF',
+                        fontSize: 14,
+                        boxWidth: 14
                     }
                 },
                 scales: {
@@ -105,8 +138,50 @@ class WhcgChart extends polymerElement_js.PolymerElement {
                         ticks: {
                             beginAtZero:true
                         }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            fontFamily: "'Exo 2', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+                            fontColor: '#FFFFFF',
+                            fontSize: 14
+                        },
+                        gridLines: {
+                            // display: true,
+                            // color: '#FFFFFF'
+                        }
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            fontFamily: "'Exo 2', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+                            fontColor: '#FFFFFF',
+                            fontSize: 14
+                        },
+                        gridLines: {
+                            // display: true,
+                            // color: '#FFFFFF'
+                        }
+                        // scaleLabel: { fontColor: '#6E6E6E', fontSize:16 }
                     }]
                 },
+                responsive: true,
+                maintainAspectRatio: false,
+                // title: {
+                //     display: true,
+                //     text: 'Title',
+                //     fontFamily: "'Exo 2', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+                //     fontColor: '#FFFFFF',
+                //     fontSize: 14
+                // },
+                layout: {
+                    padding: {
+                        left: 15,
+                        right: 15,
+                        top: 50,
+                        bottom: 20
+                    }
+                }
+                //  label: { fontSize:18 }
+                
                 // scales: {
                 //     xAxes: [{
                 //         stacked: this.stacked
@@ -123,7 +198,118 @@ class WhcgChart extends polymerElement_js.PolymerElement {
                 // }
             }
         });
+
+        // console.log("myChart");
+        // console.log(this.thechart);
+        // console.log('this');
+        // console.log(this);
+
+        // var that = this;
+
+        
+        // // 
+        // // this.thechart.update();
+
+        // setTimeout(function () {
+        //     // console.log('that');
+        //     // console.log(that);
+        //     // console.log(that.thechart);
+        //     that.thechart = new Chart(ctx, {
+        //         type: that.type,
+        //         data: data,
+
+        //         options: {
+        //             legend: {
+        //                 position: that.legendposition,
+        //                 labels: {
+        //                     fontFamily: "'Exo 2', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+        //                     fontColor: '#FFFFFF',
+        //                     fontSize: 14,
+        //                     boxWidth: 14
+        //                 }
+        //             },
+        //             scales: {
+        //                 yAxes: [{
+        //                     ticks: {
+        //                         beginAtZero: true
+        //                     }
+        //                 }],
+        //                 yAxes: [{
+        //                     ticks: {
+        //                         fontFamily: "'Exo 2', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+        //                         fontColor: '#FFFFFF',
+        //                         fontSize: 14
+        //                     },
+        //                     gridLines: {
+        //                         // display: true,
+        //                         // color: '#FFFFFF'
+        //                     }
+        //                 }],
+        //                 xAxes: [{
+        //                     ticks: {
+        //                         fontFamily: "'Exo 2', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+        //                         fontColor: '#FFFFFF',
+        //                         fontSize: 14
+        //                     },
+        //                     gridLines: {
+        //                         // display: true,
+        //                         // color: '#FFFFFF'
+        //                     }
+        //                     // scaleLabel: { fontColor: '#6E6E6E', fontSize:16 }
+        //                 }]
+        //             },
+        //             // responsive: true,
+        //             // maintainAspectRatio: false,
+        //             // title: {
+        //             //     display: true,
+        //             //     text: 'Title',
+        //             //     fontFamily: "'Exo 2', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+        //             //     fontColor: '#FFFFFF',
+        //             //     fontSize: 14
+        //             // },
+        //             layout: {
+        //                 padding: {
+        //                     left: 15,
+        //                     right: 15,
+        //                     top: 50,
+        //                     bottom: 20
+        //                 }
+        //             }
+        //             //  label: { fontSize:18 }
+
+        //             // scales: {
+        //             //     xAxes: [{
+        //             //         stacked: this.stacked
+        //             //     }],
+        //             //     yAxes: [{
+        //             //         stacked: this.stacked
+        //             //     }
+        //             //     // {
+        //             //     //     ticks: {
+        //             //     //         beginAtZero: true
+        //             //     //     }
+        //             //     // }
+        //             // ]
+        //             // }
+        //         }
+        //     });
+        // }, 1000);
+
+
+
     }
+
+    // constructor() {
+    //     super();
+
+    // }
+
+    // connectedCallback() {
+    //     super.connectedCallback();
+    //    // this._chartJs(JSON.parse(this.chartjson));
+
+       
+    // }
 }
 
 window.customElements.define('whcg-chart', WhcgChart);
